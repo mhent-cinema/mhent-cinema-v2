3ec955a33bd6eaa061b2051fb30b560f1330c6da
Tokugawa Sakura <miyazakiharuto.ent.co@gmail.com>
2026-03-05 22:13:38 +0700

    Add basic things

---
+++ data.js
@@ -0,0 +1,12 @@
+const mhentData = {
+    "star-detective": {
+        title: "Star Detective Pretty Cure!",
+        type: "series",
+        desc: "Câu chuyện về nữ sinh Akechi Anna và thám tử Kobayashi Mikuru..."
+    },
+    "kimi-to-idol": {
+        title: "Kimi to Idol Pretty Cure♪ The Movie",
+        type: "movie",
+        desc: "Uta và những người bạn được mời tham gia Lễ hội thần tượng..."
+    }
+};
