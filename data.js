b119ad76716d9d2fe89edd5f23c60f51d29dbe12
Tokugawa Sakura <miyazakiharuto.ent.co@gmail.com>
2026-03-05 22:23:27 +0700

    Add watch, direct to watch

--- data.js
+++ data.js
@@ -1,12 +1,19 @@
 const mhentData = {
-    "star-detective": {
+    "meitantei-precure": {
         title: "Star Detective Pretty Cure!",
         type: "series",
-        desc: "Câu chuyện về nữ sinh Akechi Anna và thám tử Kobayashi Mikuru..."
+        desc: "Câu chuyện về nữ sinh Akechi Anna và thám tử Kobayashi Mikuru...",
+        episodes: [
+            { title: "Tập 1", link: "https://drive.google.com/file/d/1OFWbew7CJp9-7VMhbSD80G2V7_IHtiLB/preview" },
+            { title: "Tập 2", link: "https://drive.google.com/file/d/1XHuQ04tAvJF5a8-TEWrLWbYp4mTu7M1V/preview" }
+        ]
     },
-    "kimi-to-idol": {
+    "eiga-kimi-to-idol-precure": {
         title: "Kimi to Idol Pretty Cure♪ The Movie",
         type: "movie",
-        desc: "Uta và những người bạn được mời tham gia Lễ hội thần tượng..."
+        desc: "Uta và những người bạn được mời tham gia Lễ hội thần tượng...",
+        episodes: [
+            { title: "Tập Full", link: "https://drive.google.com/file/d/1OFWbew7CJp9-7VMhbSD80G2V7_IHtiLB/preview" }
+        ]    
     }
 };
