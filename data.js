2ec1192563d061b94b2547eae5b3fda5fe442899
Tokugawa Sakura <miyazakiharuto.ent.co@gmail.com>
2026-03-05 22:37:07 +0700

    Add more information

--- data.js
+++ data.js
@@ -1,15 +1,25 @@
 const mhentData = {
     "meitantei-precure": {
         title: "Star Detective Pretty Cure!",
+        altTitle: "名探偵プリキュア！<br>Meitantei Purikyua!",
+        tag: "Phép thuật / Hành động",
+        status: "Đang tiến hành",
+        poster: "assets/Meitantei_Precure_Official_Poster.webp",
+        trailer: "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fmharuto.entertainment%2Fvideos%2F2007201789911311%2F&show_text=false&width=560&t=0",
         type: "series",
-        desc: "Câu chuyện về nữ sinh Akechi Anna và thám tử Kobayashi Mikuru...",
+        desc: "Akechi Anna là một nữ sinh 14 tuổi, học lớp 2 trung học cơ sở tại thị trấn Makoto Mirai!...",
         episodes: [
             { title: "Tập 1", link: "https://drive.google.com/file/d/1OFWbew7CJp9-7VMhbSD80G2V7_IHtiLB/preview" },
             { title: "Tập 2", link: "https://drive.google.com/file/d/1XHuQ04tAvJF5a8-TEWrLWbYp4mTu7M1V/preview" }
         ]
     },
     "eiga-kimi-to-idol-precure": {
-        title: "Kimi to Idol Pretty Cure♪ The Movie",
+        title: "Kimi to Idol Pretty Cure♪ The Movie - For You! Our KiraKilala Concert!",
+        altTitle: "映画キミとアイドルプリキュア♪お待たせ！キミに届けるキラッキライブ！<br>Eiga Kimi to Aidoru Purikyua♪ Omatase! Kimi ni Todokeru KirakkiRaibu!",
+        tag: "Phép thuật / Hành động",
+        status: "Đang tiến hành",
+        poster: "assets/Eiga_Kimi_to_Idol_Precure_Official_Poster.webp",
+        trailer: "https://www.facebook.com/plugins/video.php?height=314&href=https%3A%2F%2Fwww.facebook.com%2Fmharuto.entertainment%2Fvideos%2F2007201789911311%2F&show_text=false&width=560&t=0",
         type: "movie",
         desc: "Uta và những người bạn được mời tham gia Lễ hội thần tượng...",
         episodes: [
